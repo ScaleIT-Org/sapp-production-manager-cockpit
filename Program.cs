42cf054d507d84cf4e55eb10e923bee82a4f68a5
Nico Kuhn <kuhn@teco.edu>
2017-10-25 12:51:33 +0200

    Fixed Docker Problems (Routing Bug)

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.CommandLine;
 
 namespace productionManagerCockpit
 {
@@ -13,7 +15,12 @@ namespace productionManagerCockpit
 
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
             var host = new WebHostBuilder()
+                .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseWebRoot(Path.Combine(Directory.GetCurrentDirectory(), ".", "www"))
